e4b9eacdf2a85a39a4e7533c7ff45bc6cc853339
laujml <laura.an.la03@email.com>
2025-10-13 13:48:48 -0600

    Nueva topbar y sidebar
    Topbar: Se agrega inicio y comprar.
    Sidebar: Letra bold, cambio de colores y se agrega logo.
    Vistas: Se limpian los html y se agrega la nueva forma.
    Creacion de js con data de sidebar y topbar

---
+++ js/sidebar_data.js
@@ -0,0 +1,13 @@
+const SIDEBAR_CONTENT = `
+<div class="sidebar-logo">
+    <img src="img/loogo.png" alt="Naturally Stunning Logo" class="logo-img"> 
+    
+    </div>
+<ul>
+    <li><a href="profile.html">Mi cuenta</a></li>
+    <li><a href="productos.html">Productos</a></li>
+    <li><a href="promos.html">Promos</a></li>
+    <li><a href="contactanos.html">Contáctanos</a></li>
+    <li><a href="AcercadeNosotros.html">Acerca de</a></li>
+</ul>
+`;
---
+++ js/topbar_data.js
@@ -0,0 +1,27 @@
+const TOPBAR_HTML = `
+<header class="main-header">
+    <link rel="stylesheet" href="css/global.css">
+    <nav class="topbar-nav">
+        
+        <div class="topbar-left">
+            <label for="menu-toggle" class="menu-btn">☰</label>
+        </div>
+        
+        <div class="topbar-center">
+            
+            <div class="center-group">
+                <a href="index.html" class="nav-link">Inicio</a>
+                
+                <a href="productos.html" class="nav-link">Comprar</a>
+            </div>
+            
+        </div>
+        
+        <div class="topbar-right">
+            <a href="carrito.html" class="icon-link">🛒</a>
+            <a href="perfil.html" class="icon-link">👤</a>
+        </div>
+        
+    </nav>
+</header>
+`;
